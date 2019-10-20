fadf58e1a8ff81f20ddf9c15b83fc4a0e1717470
nahsiloh <lishan1512@gmail.com>
2019-10-20 18:59:09 +0800

    created browserRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./App.css";
-import CurrencyConverter from "./common/currency_converter";
-import DatePicker from "./components/date_picker";
 import TripSelector from "./components/trip_selector";
+import Login from "./components/login";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import AppRouter from "./components/header";
 import ExistingTrip from "./components/existing_trip";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets plan your next trip!</h1>
-      <TripSelector />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Lets fly away!</h1>
+
+        <AppRouter />
+        <Switch>
+          <Route exact path="/" component={() => <Login />} />
+          <Route exact path="/selecttrip" component={() => <TripSelector />} />
+          <Route
+            exact
+            path="/existingtrip"
+            component={() => <ExistingTrip />}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
