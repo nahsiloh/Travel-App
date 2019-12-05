771ecfd524f13a8afe9f26b9903193b01002ee2b
nahsiloh <lishan1512@gmail.com>
2019-12-05 22:08:31 +0800

    add trip selector on and change css of navbar

--- src/components/Navbar/navBar.js
+++ src/components/Navbar/navBar.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./navBar.css";
 
 function NavBar(props) {
   return (
     <div>
       <header data-testid="Navbar">
+        <Link to="/tripSelect">Trip Selector</Link>
         <Link to="/" onClick={props.logout}>
           Logout
         </Link>
