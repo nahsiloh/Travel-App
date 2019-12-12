f6a972226797f795edf5d511e55af6029a981ac4
nahsiloh <lishan1512@gmail.com>
2019-12-12 14:47:26 +0800

    change baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: process.env.REACT_APP_BASE_URL
-  baseURL: "http://localhost:5000"
+  baseURL: process.env.REACT_APP_BASE_URL
 });
