683542f0436b387b930d499bb7353cbd4e878229
nahsiloh <lishan1512@gmail.com>
2023-03-28 22:14:03 +0800

    refactor DateSelector component

---
+++ src/components/DateSelector/index.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useRef, useEffect, useReducer } from "react";
+
+import { Datepicker } from "@datepicker-react/styled";
+import { isBefore } from "date-fns";
+import { Button, Overlay } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import { formatDate } from "../utils";
+
+import reducer from "../../reducer";
+import { initialState } from "../../reducer/state";
+
+import { ButtonStyles } from "../../UIComponents/styles";
+import { updateStartDate, updateEndDate } from "../../reducer/actions";
+
+const DateSelector: React.FC = () => {
+  const target = useRef(null);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { tripStartDate, tripEndDate } = state;
+
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [focusedInput, setFocusedInput] = useState<
+    "startDate" | "endDate" | null
+  >("startDate");
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+
+  useEffect(() => {
+    if (selectedDate !== null) {
+      if (isBefore(selectedDate, tripEndDate) === true) {
+        setFocusedInput("startDate");
+        dispatch(updateStartDate(selectedDate));
+      } else {
+        setFocusedInput("endDate");
+        dispatch(updateEndDate(selectedDate));
+      }
+    }
+  }, [selectedDate, tripEndDate, tripStartDate]);
+
+  const handleDatePicker = () => {
+    setIsDatePickerOpen(!isDatePickerOpen);
+  };
+
+  const handleDatePickerClose = () => {
+    setIsDatePickerOpen(false);
+  };
+
+  return (
+    <div>
+      <Row className="mb-4" style={{ padding: 0 }}>
+        <Col>
+          <h4>Start Date</h4>
+          <Button
+            ref={target}
+            size="lg"
+            style={{ ...ButtonStyles }}
+            onClick={handleDatePicker}
+          >
+            {formatDate(tripStartDate)}
+          </Button>
+        </Col>
+        <Col>
+          <h4>End Date</h4>
+          <Button
+            ref={target}
+            size="lg"
+            style={{ ...ButtonStyles }}
+            onClick={handleDatePicker}
+          >
+            {formatDate(tripEndDate)}
+          </Button>
+        </Col>
+      </Row>
+
+      <Overlay
+        target={target.current}
+        show={isDatePickerOpen}
+        placement="bottom"
+      >
+        {({ ...props }) => (
+          <div
+            {...props}
+            style={{
+              position: "absolute",
+              marginTop: 10,
+              marginLeft: "auto",
+              marginRight: "auto",
+              ...props.style,
+            }}
+          >
+            <Datepicker
+              startDate={tripStartDate}
+              endDate={tripEndDate}
+              onClose={handleDatePickerClose}
+              onDatesChange={({ startDate }) => {
+                setSelectedDate(startDate);
+              }}
+              focusedInput={focusedInput}
+              numberOfMonths={2}
+              showSelectedDates={false}
+              showResetDates={false}
+            />
+          </div>
+        )}
+      </Overlay>
+    </div>
+  );
+};
+
+export default DateSelector;
