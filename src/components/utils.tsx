66258041400bcb4a63fffb02ac578dd4414c5187
nahsiloh <lishan1512@gmail.com>
2023-04-02 17:28:26 +0800

    added formatItinerary utils

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -1,4 +1,5 @@
 import { format } from "date-fns";
+import { ItineraryByDate, ItineraryItem } from "./types";
 
 export const formatDate = (date: Date) => {
   return format(date, "d LLL yyyy");
@@ -10,4 +11,20 @@ export const formatDay = (date: Date) => {
 
 export const formatStringToDate = (date: string) => {
   return new Date(date);
+};
+
+export const formatItinerary = (itinerary: ItineraryItem[]) => {
+  const formattedItinerary: ItineraryByDate = {};
+
+  itinerary.forEach((item) => {
+    const { date } = item;
+    const formattedDate = formatDate(new Date(date));
+    if (formattedItinerary[formattedDate] === undefined) {
+      formattedItinerary[formattedDate] = [item];
+    } else {
+      formattedItinerary[formattedDate].push(item);
+    }
+  });
+
+  return formattedItinerary;
 };
