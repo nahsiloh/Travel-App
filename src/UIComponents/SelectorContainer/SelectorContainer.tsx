2a246ff61eda009dc10105ff4f41b4d2b320698e
nahsiloh <lishan1512@gmail.com>
2023-04-02 17:31:02 +0800

    include id and style props for selector container

--- src/UIComponents/SelectorContainer/SelectorContainer.tsx
+++ src/UIComponents/SelectorContainer/SelectorContainer.tsx
@@ -3,16 +3,24 @@ import React, { ReactElement } from "react";
 import "./SelectorContainer.css";
 
 type SelectorContainerProps = {
-  selectorHeader: string;
+  id?: string;
+  selectorHeader: string | ReactElement;
   selectorForm: ReactElement;
+  style?: object;
 };
 
 const SelectorContainer: React.FC<SelectorContainerProps> = ({
+  id,
   selectorHeader,
   selectorForm,
+  style,
 }) => {
   return (
-    <div className="selector_container">
+    <div
+      className="selector_container"
+      id={id}
+      style={{ marginBottom: 20, ...style }}
+    >
       <h2 className="selectorContainer__heading">{selectorHeader}</h2>
       <section className="selectorContainer__form">{selectorForm}</section>
     </div>
